3fad43d7b3df38d83d8ad43b8f789cac21bb5d32
IzaiAnda <44356898+IzaiAnda@users.noreply.github.com>
2020-12-22 12:39:51 +0100

    Admin
    Comments in admins post

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment, Category,Contributors
 
-admin.site.register(Post)
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 2
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+    list_display = ('title','created_date', 'published_date')
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Contributors)
